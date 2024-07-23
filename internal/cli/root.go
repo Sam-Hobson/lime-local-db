e20b4f0f8960f87eaedc1926031c598cbde60b95
sam hobson <shhobson@proton.me>
2024-07-23 20:19:53 +1000

    Added proper selection flags for db/column

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,13 +1,28 @@
 package cli
 
 import (
+	"github.com/sam-hobson/internal/operations"
 	"github.com/spf13/cobra"
 )
+
+var selectedDb = ""
+var selectedCol = ""
 
 var rootCmd = &cobra.Command{
 	Use:   "limedb",
 	Short: "An application for interacting with a simple database",
 	Long:  "TODO: This",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+        if cmd.PersistentFlags().Lookup("db").Changed {
+            operations.SelectDb(selectedDb)
+        }
+
+        if selectedCol != "" {
+            operations.SelectCol(selectedCol)
+        }
+
+        return nil
+	},
 }
 
 func ProcessArgs() error {
@@ -15,9 +30,13 @@ func ProcessArgs() error {
 }
 
 func init() {
+	globalFlags := rootCmd.PersistentFlags()
+
+	globalFlags.StringVar(&selectedDb, "db", "", "Selected database to use for operations")
+	globalFlags.StringVar(&selectedCol, "col", "", "Selected column to use for operations")
+
 	rootCmd.AddCommand(SetupCommand)
+
 	rootCmd.AddCommand(NewDbCommand)
 	rootCmd.AddCommand(RmDbCommand)
-    rootCmd.AddCommand(SelDbCommand)
-    rootCmd.AddCommand(SelColumnCommand)
 }
