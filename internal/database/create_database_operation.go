fbb41a73658096137ab8db86e9673c8a6633583e
sam hobson <shhobson@proton.me>
2024-07-26 21:21:03 +1000

    Remove unecessary code

--- internal/database/create_database_operation.go
+++ internal/database/create_database_operation.go
@@ -24,18 +24,18 @@ func CreateDatabase(databaseName string, columns []*Column) error {
 		fileName += ".db"
 	}
 
-    relFs := util.NewRelativeFsManager(viper.GetString("limedbHome"))
+	relFs := util.NewRelativeFsManager(viper.GetString("limedbHome"))
 
-    if exists, err := relFs.FileExists("stores", fileName); err != nil {
-        return err
-    } else if exists {
+	if exists, err := relFs.FileExists("stores", fileName); err != nil {
+		return err
+	} else if exists {
 		slog.Error("Cannot create a new database as it already exists.", "log_code", "6c95edf6", "database_name", databaseName)
 		return errors.Errorf("Cannot create a new database as it already exists")
-    }
+	}
 
-    if err := relFs.CreateFile("stores", fileName); err != nil {
-        return err
-    }
+	if err := relFs.CreateFile("stores", fileName); err != nil {
+		return err
+	}
 
 	dbPath := relFs.FullPath("stores", fileName)
 	db, err := sql.Open("sqlite3", dbPath)
@@ -52,7 +52,7 @@ func CreateDatabase(databaseName string, columns []*Column) error {
 	for _, col := range columns {
 		opts := make([]string, 10)
 		opts = append(opts, col.ColName)
-        opts = append(opts, col.DataType.String())
+		opts = append(opts, col.DataType.String())
 
 		if col.NotNull {
 			opts = append(opts, "NOT NULL")
@@ -73,14 +73,12 @@ func CreateDatabase(databaseName string, columns []*Column) error {
 
 	slog.Info("Creating table with SQL command.", "log_code", "0cb6a54d", "SQL", createTableStr)
 
-	_, err = db.Exec(createTableStr, args...)
-
-	if err != nil {
+	if _, err = db.Exec(createTableStr, args...); err != nil {
 		slog.Error("Failed executing create table command.", "log_code", "fed4e102", "SQL", createTableStr)
 		return err
 	}
 
-	slog.Info("Successfully created a new-db.", "log_code", "7bf9634b", "Db_path", dbPath)
+	slog.Info("Successfully created a new database.", "log_code", "7bf9634b", "Db_path", dbPath)
 	return err
 
 }
